885f5daedd29f9cb604b90818c46b0c96c960b26
Fatime.R <93155187+faru323@users.noreply.github.com>
2022-12-04 18:50:02 +0400

    upload cards

---
+++ elonmusk/index.js
@@ -0,0 +1,25 @@
+const totalMoney = parseInt(document.getElementById('money__count').innerHTML);
+const sellButton=document.getElementsByClassName('sellBtn').innerHTML;
+const buyButton=document.getElementsByClassName('buyBtn').innerHTML;
+const spentMoney=parseInt(document.getElementsByTagName('p').innerHTML)
+console.log(totalMoney,sellButton)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
